857988a4dc86e99d251ceae899becc17ecf7a0a8
henry4912 <henryha4912@gmail.com>
2023-05-08 15:27:16 -0700

    Added code to open files in directories

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import os
+from bs4 import BeautifulSoup
 
+def run():
+    assignmentFile =  os.getcwd() + '/DEV'
+    for directories in os.listdir(assignmentFile):
+        for file in os.path.join(assignmentFile, directories):
+            with open(os.path.join(assignmentFile, file), 'r') as json:
+                soup = BeautifulSoup(json, 'lxml')
+                print(soup.contents)
+            break
+        break
