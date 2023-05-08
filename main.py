081a0138ca8493528b6f2671f4dcd6cbb4ecbda2
henry4912 <henryha4912@gmail.com>
2023-05-08 15:53:27 -0700

    Fixed file reading loops

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
 from bs4 import BeautifulSoup
 
+
 def run():
-    assignmentFile =  os.getcwd() + '/DEV'
-    for directories in os.listdir(assignmentFile):
-        for file in os.path.join(assignmentFile, directories):
-            with open(os.path.join(assignmentFile, file), 'r') as json:
+    assignmentFolder = os.getcwd() + '\ANALYST'
+    # print(assignmentFolder)
+    for directories in os.listdir(assignmentFolder):
+        # print(directories)
+        subfolder = os.path.join(assignmentFolder, directories)
+        for file in os.listdir(os.path.join(assignmentFolder, directories)):
+            # print(os.path.join(assignmentFolder, directories))
+            print(os.path.join(subfolder, file))
+            with open(os.path.join(subfolder, file), 'r') as json:
                 soup = BeautifulSoup(json, 'lxml')
                 print(soup.contents)
             break
         break
+
+
+run()
