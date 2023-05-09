8962d5036ce10072ae968970694ecd84034c23d4
henry4912 <henryha4912@gmail.com>
2023-05-09 15:16:49 -0700

    Added token freq dictionary, tokenizer, and json file reading

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 from bs4 import BeautifulSoup
+import json
+import re
 
 
 def run():
+    counter = 0
+    frequencies = {}
+
     assignmentFolder = os.getcwd() + '\ANALYST'
     # print(assignmentFolder)
     for directories in os.listdir(assignmentFolder):
@@ -10,12 +15,31 @@ def run():
         subfolder = os.path.join(assignmentFolder, directories)
         for file in os.listdir(os.path.join(assignmentFolder, directories)):
             # print(os.path.join(assignmentFolder, directories))
-            print(os.path.join(subfolder, file))
-            with open(os.path.join(subfolder, file), 'r') as json:
-                soup = BeautifulSoup(json, 'lxml')
-                print(soup.contents)
-            break
-        break
+            # print(os.path.join(subfolder, file))
+            with open(os.path.join(subfolder, file), 'r') as j:
+                soup = BeautifulSoup(j, 'lxml')
+                jsonContents = json.loads(soup.text)
+                currURL = jsonContents['url']
+                tokens = tokenizer(jsonContents['content'])
+                # print(jsonContents['content'])
+                for t in tokens:
+                    if t in frequencies.keys():
+                        if currURL in frequencies[t].keys():
+                            frequencies[t][currURL] += 1
+                        else:
+                            frequencies[t] = {}
+                            frequencies[t][currURL] = 1
+                    else:
+                        frequencies[t] = {}
+                        frequencies[t][currURL] = 1
+
+            counter += 1
+            print(counter)
+
+
+def tokenizer(contents):
+    tokens = re.findall(r'[a-z0-9]+', contents)
+    return tokens
 
 
 run()
