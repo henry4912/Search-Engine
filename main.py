8932453f5030477964fc45a636a50acb7e47badd
henry4912 <henryha4912@gmail.com>
2023-05-11 10:54:34 -0700

    Deleted stopwords

--- main.py
+++ main.py
@@ -36,15 +36,14 @@ def run():
                         tokens = tokenizer(soup.text)
                         for t in tokens:
                             t = porterStem.stem(t)
-                            if not is_stopword(t):
-                                if t in frequencies.keys():
-                                    if currURL in frequencies[t].keys():
-                                        frequencies[t][currURL] += 1
-                                    else:
-                                        frequencies[t][currURL] = 1
+                            if t in frequencies.keys():
+                                if currURL in frequencies[t].keys():
+                                    frequencies[t][currURL] += 1
                                 else:
-                                    frequencies[t] = {}
                                     frequencies[t][currURL] = 1
+                            else:
+                                frequencies[t] = {}
+                                frequencies[t][currURL] = 1
 
     f = open("fixedJSONOutput.txt", "w+")
     f.write('valid json: ' + str(validJsonCounter) + '\n')
@@ -79,34 +78,6 @@ def run():
 def tokenizer(contents):
     tokens = re.findall(r'[a-z0-9]+', contents.lower())
     return tokens
-
-
-def is_stopword(token):
-    stopWords = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 'aren\'t',
-                 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by',
-                 'can\'t', 'cannot', 'could', 'couldn\'t', 'did', 'didn\'t', 'do', 'does', 'doesn\'t', 'doing',
-                 'don\'t',
-                 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', 'hadn\'t', 'has', 'hasn\'t', 'have',
-                 'haven\'t', 'having', 'he', 'he\'d', 'he\'ll', 'he\'s', 'her', 'here', 'here\'s', 'hers', 'herself',
-                 'him', 'himself', 'his', 'how', 'how\'s', 'i', 'i\'d', 'i\'ll', 'i\'m', 'i\'ve', 'if', 'in', 'into',
-                 'is', 'isn\'t', 'it', 'it\'s', 'its', 'itself', 'let\'s', 'me', 'more', 'most', 'mustn\'t', 'my',
-                 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours',
-                 'ourselves', 'out', 'over', 'own', 'same', 'shan\'t', 'she', 'she\'d', 'she\'ll', 'she\'s', 'should',
-                 'shouldn\'t', 'so', 'some', 'such', 'than', 'that', 'that\'s', 'the', 'their', 'theirs', 'them',
-                 'themselves', 'then', 'there', 'there\'s', 'these', 'they', 'they\'d', 'they\'ll', 'they\'re',
-                 'they\'ve',
-                 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', 'wasn\'t', 'we',
-                 'we\'d',
-                 'we\'ll', 'we\'re', 'we\'ve', 'were', 'weren\'t', 'what', 'what\'s', 'when', 'when\'s', 'where',
-                 'where\'s',
-                 'which', 'while', 'who', 'who\'s', 'whom', 'why', 'why\'s', 'with', 'won\'t', 'would', 'wouldn\'t',
-                 'you',
-                 'you\'d', 'you\'ll', 'you\'re', 'you\'ve', 'your', 'yours', 'yourself', 'yourselves']
-
-    if token in stopWords:
-        return True
-    else:
-        return False
 
 if __name__ == '__main__':
     run()
