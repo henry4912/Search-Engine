bb480814c32245dcc1ad4e3f7817e835a29b66a5
henry4912 <henryha4912@gmail.com>
2023-05-12 19:07:20 -0700

    Moved file writing, changed parser,  added comments

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ import json
 import re
 from nltk.stem import PorterStemmer
 
+'''
+Loops through the directories and files under DEV to read and parse the json files.
+Converts the json file into a dictionary and assigns the url a unique document ID.
+Reads the contents of the page and stems and tokenizes the document.
+The indexer is a dictionary with the key being the token and the value being
+another dictionary that holds the document ID as the key and the frequency of the token
+in that document as the value.
+
+
+Source for reading into directories: https://stackoverflow.com/questions/39508469/reading-all-the-file-content-in-directory
+Source for reading json into dictionary: https://www.geeksforgeeks.org/convert-json-to-dictionary-in-python/
+'''
 def run():
     porterStem = PorterStemmer()
     idCounter = 1
     docID = {}
     validJsonCounter = 0  # valid json files
-    # validURL = [] #list of valid urls (the url in the json file)
-    invalidJsonCounter = 0  # invalid json files
     frequencies = {}  # token frequencies
-    badURL = []  # invalid json files
-    # testingLimit = 0 #amount of json files to test
-    assignmentFolder = os.getcwd() + '/ANALYST'
-    # print(assignmentFolder)
+    assignmentFolder = os.getcwd() + '/DEV'
     for directories in os.listdir(assignmentFolder):
-        # print(directories)
         subfolder = os.path.join(assignmentFolder, directories)
-        # while testingLimit < 10:
         for file in os.listdir(os.path.join(assignmentFolder, directories)):
-            # print(os.path.join(assignmentFolder, directories))
-            # print(os.path.join(subfolder, file))
-            s = os.path.join(subfolder, file)
             with open(os.path.join(subfolder, file), 'r') as j:
-                # testingLimit = testingLimit + 1
                 jsonContentDictionary = json.load(j)
                 currURL = ''
                 for key, value in jsonContentDictionary.items():
@@ -34,7 +35,7 @@ def run():
                         validJsonCounter = validJsonCounter + 1
                         docID[currURL] = idCounter
                     if key == 'content':
-                        soup = BeautifulSoup(value, 'xml')
+                        soup = BeautifulSoup(value, 'html.parser')
                         tokens = tokenizer(soup.text)
                         for t in tokens:
                             t = porterStem.stem(t)
@@ -48,47 +49,27 @@ def run():
                                 frequencies[t][idCounter] = 1
                     
                 idCounter += 1
+    reportWrite(idCounter, validJsonCounter, frequencies)
 
-    f = open("fixedJSONOutput.txt", "w+")
-
-    f.write('\nNumber of documents: ' + str(idCounter))
-    f.write('\nNumber of unique tokens: ' + str(len(frequencies)))
-    indexSize = sys.getsizeof(frequencies) / 1000
-    f.write("\nTotal size of index on disk: " + str(indexSize) + "KB")
-
-    f.write('valid json: ' + str(validJsonCounter) + '\n')
-    f.write('invalid json: ' + str(invalidJsonCounter))
-    f.write('\nEvery bad json file: \n')
-
-    for link in badURL:
-        f.write(link)
-        f.write('\n')
-    """
-    f.write('\nEvery valid URL: \n')
-
-
-    for link in validURL:
-        f.write(link)
-        f.write('\n')
-    """
-  
-    f.write('\n\n\n\nFrequencies Dictionary:\n')
-    for keys, values in frequencies.items():
-        f.write('token: ' + str(keys))
-        f.write('\ntoken URL count: ' + str(len(values)) + '\n')
-        # for key, value in values.items():
-        #    f.write('%s       %s' % (key, value))
-        #    f.write('\n')
-
-    # print(frequencies)
-
-   
-
-
-
+'''
+Tokenizes the contents of the files into alphanumeric tokens
+'''
 def tokenizer(contents):
     tokens = re.findall(r'[a-z0-9]+', contents.lower())
     return tokens
+
+'''
+Writes the report with the number of indexed documents, unique tokens,
+and size of the indexer
+'''
+def reportWrite(idCounter, validJsonCounter, frequencies):
+    f = open("report.txt", "w+")
+    f.write('Report:\n\n')
+    f.write('\nNumber of documents: ' + str(idCounter))
+    f.write('\nNumber of Indexed Documents: ' + str(validJsonCounter))
+    f.write('\nNumber of unique tokens: ' + str(len(frequencies)))
+    indexSize = sys.getsizeof(frequencies) / 1000
+    f.write("\nTotal size of index on disk: " + str(indexSize) + " KB")
 
 if __name__ == '__main__':
     run()
