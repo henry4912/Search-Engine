95c84b86ad5b6f0d5e7ada539097c9b39cfd4094
henry4912 <henryha4912@gmail.com>
2023-06-01 14:47:49 -0700

    Separated indexing and searching into query.py

--- main.py
+++ main.py
@@ -76,6 +76,10 @@ def run():
         ind = writeToFile(frequencies, len(bitIndexes))
         bitIndexes.append(ind)
 
+    with open('docID.json', 'w+') as d:
+        json.dump(docID, d)
+        d.close()
+
     # if len(bitIndexes) > 1:
     #    mergeFiles(bitIndexes, totalFileCount)
     mergeFiles(bitIndexes, totalFileCount)
@@ -85,7 +89,7 @@ def run():
     del frequencies
     del fileCount
 
-    search(frequencies, docID)
+    print('Done Indexing!')
 
 
 '''
@@ -99,68 +103,6 @@ def tokenizer(contents):
 
 
 # https://stackoverflow.com/questions/36367527/centering-widgets-in-tkinter-frame-using-pack
-def search(freq, docID):
-    window = Tk()
-    window.title('Yet Another Search Engine')
-    window.geometry('1000x700')
-    labelTitle = Label(window, text='Search Engine')
-    labelTitle.place(relx=0.5, rely=0.2, anchor=CENTER)
-
-    frame1 = Frame(window)
-    labelResults = Label(frame1)
-    labelResults.pack(side='bottom')
-    frame1.place(relx=0.5, rely=0.6, anchor=S)
-
-    frame2 = Frame(window)
-    searchBox = Entry(frame2, width=30)
-    searchBox.pack(side='left')
-
-    f = open('reportM2.txt', 'w+')
-    f.write('Student Names: Derrick Jones, Sabrina Yang, Henry Ha, Christine Nguyen\n')
-    f.write('Student IDs: 93547582, 27422353, 25602171, 56965805\n\n')
-    f.write('Report M2:\n\n')
-    f.close()
-
-    def clicked():
-        results = findResults(searchBox.get().lower(), freq, docID)
-        reportWriteM2(searchBox.get(), results)
-        labelResults.configure(text=results)
-
-    search = Button(frame2, text='Search', command=clicked)
-    search.pack(side='left')
-    frame2.place(relx=0.5, rely=0.25, anchor=N)
-
-    window.mainloop()
-
-
-def findResults(text, freq, docID):
-    sets = []
-    results = ''
-    porterStem = PorterStemmer()
-    tokens = text.split()
-    tokens = [porterStem.stem(t) for t in tokens]
-
-    for token in tokens:
-        if token not in freq.keys():
-            return 'No results found'
-        else:
-            urlFreq = freq[token]
-            urls = []
-            for urlID in urlFreq.keys():
-                urls.append(docID[urlID])
-            sets.append(set(urls))
-
-    intersection = list(set.intersection(*sets))
-    if len(intersection) == 0:
-        return 'No results found'
-    elif len(intersection) > 5:
-        for url in intersection[:5]:
-            results += str(url) + '\n\n'
-    else:
-        for url in urlFreq:
-            results += str(url) + '\n\n'
-
-    return results
 
 
 def writeToFile(index, writeCount):
@@ -188,7 +130,6 @@ def writeToFile(index, writeCount):
     return bitIndex
 
 
-# https://stackoverflow.com/questions/7945182/opening-multiple-an-unspecified-number-of-files-at-once-and-ensuring-they-are
 def mergeFiles(bitIndexes, totalFileCount):
     openFiles = []
     files = []
@@ -262,7 +203,9 @@ def mergeFiles(bitIndexes, totalFileCount):
         for f in openFiles:
             f.close()
 
-    return finalIndex
+    with open('final-index.json', 'w+') as f:
+        json.dump(finalIndex, f)
+        f.close()
 
 
 def mergeDictionaries(dicts):
